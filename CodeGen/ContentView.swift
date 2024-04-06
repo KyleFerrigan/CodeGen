1b8fcde9f022a6eb929306a83d6455a7cbe477c6
Kyle Ferrigan <work@kyleferrigan.com>
2024-04-06 18:22:24 -0500

    Disabled autocorrect on datai nput field

--- CodeGen/ContentView.swift
+++ CodeGen/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
                     }
                 
                 TextField("Insert data here", text: $textIn)
+                    .autocorrectionDisabled()
                 NavigationLink(destination: FavoritesView()) {
                           Text("Favorited QR Codes")
                         }
