9ba14883191eb7eb3afee19ee6d84eb5ba036089
Kyle Ferrigan <work@kyleferrigan.com>
2024-04-07 08:46:32 -0500

    Changed creation of model container to be its own function outside of view, changed accent color slightly to reduce redundancy

--- CodeGen/CodeGenApp.swift
+++ CodeGen/CodeGenApp.swift
@@ -15,46 +15,9 @@ import CoreData
 
 @main
 struct CodeGenApp: App {
-    @AppStorage("userAccentChoice") var userAccentChoice: Int = 0
-    
-    //MARK: Model Container
-    var FavQRCodes: ModelContainer = {
-        let schema = Schema([QRCode.self,])
-        let config = ModelConfiguration(for: QRCode.self, isStoredInMemoryOnly: false)
-        do {
-            if isICloudContainerAvailable(){
-                // MARK: CloudKit Container Only
-                // Use an autorelease pool to make sure Swift deallocates the persistent
-                // container before setting up the SwiftData stack.
-                try autoreleasepool {
-                    let desc = NSPersistentStoreDescription(url: config.url)
-                    let opts = NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.CodeGen.QRCodes")
-                    desc.cloudKitContainerOptions = opts
-                    // Load the store synchronously so it completes before initializing the
-                    // CloudKit schema.
-                    desc.shouldAddStoreAsynchronously = false
-                    if let mom = NSManagedObjectModel.makeManagedObjectModel(for: [QRCode.self]) {
-                        let container = NSPersistentCloudKitContainer(name: "QR Codes", managedObjectModel: mom)
-                        container.persistentStoreDescriptions = [desc]
-                        container.loadPersistentStores {_, err in
-                            if let err {
-                                fatalError(err.localizedDescription)
-                            }
-                        }
-                        // Initialize the CloudKit schema after the store finishes loading.
-                        try container.initializeCloudKitSchema()
-                        // Remove and unload the store from the persistent container.
-                        if let store = container.persistentStoreCoordinator.persistentStores.first {
-                            try container.persistentStoreCoordinator.remove(store)
-                        }
-                    }
-                }
-            } // MARK: CloudKit END
-            return try ModelContainer(for: schema, configurations: config)
-        } catch {
-            fatalError(error.localizedDescription)
-        }
-    }()
+    @AppStorage("userAccentChoice") var userAccentChoice: Int = -1
+
+    var favQRCodes: ModelContainer = createQRCloudContainer()
 
     // MARK: App
     var body: some Scene {
@@ -62,34 +25,70 @@ struct CodeGenApp: App {
             ContentView()
                 .accentColor(accentColorCheck())
         }
-        .modelContainer(FavQRCodes)
+        .modelContainer(favQRCodes)
     }
     
     //MARK: Check Accent Color
     func accentColorCheck()->Color{
         switch userAccentChoice {
         case 0:
-            return .accentColor
-        case 1:
             return .red
-        case 2:
+        case 1:
             return .green
-        case 3: 
+        case 2:
             return .blue
-        case 4:
+        case 3:
             return .orange
-        case 5:
+        case 4:
             return .purple
-        case 6:
+        case 5:
             return .yellow
-        case 7:
+        case 6:
             return .mint
-        case 8:
+        case 7:
             return .gray
-            
         default:
             return .accentColor
         }
+    }
+}
+
+//MARK: Model Container
+func createQRCloudContainer() -> ModelContainer {
+    let schema = Schema([QRCode.self,])
+    let config = ModelConfiguration(for: QRCode.self, isStoredInMemoryOnly: false)
+    do {
+        if isICloudContainerAvailable(){
+            // MARK: CloudKit Container Only
+            // Use an autorelease pool to make sure Swift deallocates the persistent
+            // container before setting up the SwiftData stack.
+            try autoreleasepool {
+                let desc = NSPersistentStoreDescription(url: config.url)
+                let opts = NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.CodeGen.QRCodes")
+                desc.cloudKitContainerOptions = opts
+                // Load the store synchronously so it completes before initializing the
+                // CloudKit schema.
+                desc.shouldAddStoreAsynchronously = false
+                if let mom = NSManagedObjectModel.makeManagedObjectModel(for: [QRCode.self]) {
+                    let container = NSPersistentCloudKitContainer(name: "QR Codes", managedObjectModel: mom)
+                    container.persistentStoreDescriptions = [desc]
+                    container.loadPersistentStores {_, err in
+                        if let err {
+                            fatalError(err.localizedDescription)
+                        }
+                    }
+                    // Initialize the CloudKit schema after the store finishes loading.
+                    try container.initializeCloudKitSchema()
+                    // Remove and unload the store from the persistent container.
+                    if let store = container.persistentStoreCoordinator.persistentStores.first {
+                        try container.persistentStoreCoordinator.remove(store)
+                    }
+                }
+            }
+        } // MARK: CloudKit END
+        return try ModelContainer(for: schema, configurations: config)
+    } catch {
+        fatalError(error.localizedDescription)
     }
 }
 
